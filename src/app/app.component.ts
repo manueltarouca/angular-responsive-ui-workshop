332b37b443d6dea63d4b17da11cbd838a7b47692
Tarouca, Manuel Martins <manuemartins@deloitte.pt>
2022-05-13 11:45:14 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ft5-angular-workshop-01';
+}
